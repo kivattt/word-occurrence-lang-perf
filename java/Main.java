f0e55b871e42fde442ab73e8260ff72d26aab23e
kivattt <kivattt@gmail.com>
2024-01-07 22:08:42 +0100

    Remove java comments

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,7 @@
-//package org.example;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
 	public static void usage(){
 		System.out.println("Usage: java Main <word> <filename>");
